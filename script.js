22094420d68e0cebada1697a86e10924b6f13c24
Sasha <alexxmyshak@gmail.com>
2020-05-22 23:15:49 +0300

    New home wor2

--- script.js
+++ script.js
@@ -30,12 +30,79 @@ let personalMovieDB = {
     genres: [],
     privat: false};
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-b = prompt('На сколько оцените его?', ''),
-c = prompt('Один из последних просмотренных фильмов?', ''),
-d = prompt('На сколько оцените его?', '');
+// let a = prompt('Один из последних просмотренных фильмов?', ''),
+// b = prompt('На сколько оцените его?', ''),
+// c = prompt('Один из последних просмотренных фильмов?', ''),
+// d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
+let i = 0;
+do {
+   let a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+        a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('something wrong');
+        i--;
+    }
+    i++;
+} while (i < 2);
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+//         a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         console.log('something wrong');
+//         i--;
+//     }
+//     i++;
+// } ;
+
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null &&
+//         a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         console.log('something wrong');
+//         i--;
+//     };
+    
+
+
+/* 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка" */
+if ( numberOfFilms < 10) { 
+    alert ("Просмотрено довольно мало фильмов");
+} else if ( numberOfFilms >= 10 &&  numberOfFilms < 30) {
+        alert ("Вы классический зритель");
+ } else if ( numberOfFilms >= 30){
+     alert("Вы киноман");
+ } else {
+     alert("Произошла ошибка");}
